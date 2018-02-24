d7e6c0aad14fc91ca336a474616fa6bbcbff339a
zelon88 <zelon88@gmail.com>
2018-02-24 00:57:12 -0500

    v0.8 - Initial commit.
    -v0.8.
    -Back-end alpha based on previously stable code is in the testing phase.
    -Front-end is still under-development.
    -Not fully functional yet.
    -Logging works.
    -Delete timers aren't using config.php parameters yet.
    -No stylesheets yet.
    -Drag-and-drop works courtesy of dropzone.js (https://github.com/enyo/dropzone).

---
+++ Resources/HRC2-Functions.js
@@ -0,0 +1,8 @@
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+     e.style.display = 'none';
+  else
+     e.style.display = 'block'; }
+function goBack() {
+  window.history.back(); }

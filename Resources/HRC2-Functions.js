adafced90b17fd54047b66a5c8cfa2ad4217851e
zelon88 <zelon88@gmail.com>
2018-02-25 01:06:32 -0500

    v0.8.2 - Update GUI, Fix bugs, prep for uploading.
    -v0.8.2.
    -Update GUI to properly show more info.
    -Update CSS to properly display buttons.
    -Tweak logic.
    -Preparing for uploading files.

--- Resources/HRC2-Functions.js
+++ Resources/HRC2-Functions.js
@@ -1,8 +1,13 @@
+// / The following code toggles the visisbility of the selected object between block and none.
 function toggle_visibility(id) {
   var e = document.getElementById(id);
-  if(e.style.display == 'block')
-     e.style.display = 'none';
+  if(e.style.display == 'none')
+     e.style.display = 'block';
   else
-     e.style.display = 'block'; }
+     e.style.display = 'none'; }
+
+// / The following code gives a nice onclick function for making a back button.
 function goBack() {
   window.history.back(); }
+
+

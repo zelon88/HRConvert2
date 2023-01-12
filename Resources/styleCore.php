cdf29e16fdd126f634a7ae83000875291dbf2512
Justin Grimes <zelon88@gmail.com>
2023-01-12 02:19:54 -0500

    v3.1.2 - Improve image support. $AllowStreams config entry, bump JQuery, add some icons.
    -v3.1.2.
    -Improve Installation_Instructions.txt.
    -Revise 3D Model support, Vector file support, Image support, Iso support.
    -Improve securePath function comments, add URL support.
    -Trying to automate the translation into other languages.
    -Add rc-local.service template file for systems where it is missing and needs to be installed.

--- Resources/styleCore.php
+++ Resources/styleCore.php
@@ -1,7 +1,7 @@
 <?php
 // / -----------------------------------------------------------------------------------
 // / APPLICATION INFORMATION ...
-// / HRConvert2, Copyright on 4/11/2022 by Justin Grimes, www.github.com/zelon88
+// / HRConvert2, Copyright on 1/10/2023 by Justin Grimes, www.github.com/zelon88
 // /
 // / LICENSE INFORMATION ...
 // / This project is protected by the GNU GPLv3 Open-Source license.
@@ -20,8 +20,8 @@
 // /
 // / DEPENDENCY REQUIREMENTS ... 
 // / This application requires Debian Linux (w/3rd Party audio license), 
-// / Apache 2.4, PHP 7+, LibreOffice, Unoconv, ClamAV, Tesseract, Rar, Unrar, Unzip, 
-// / 7zipper, FFMPEG, PDFTOTEXT, Dia, PopplerUtils, MeshLab, mkisofs & ImageMagick.
+// / Apache 2.4, PHP 8+, LibreOffice, Unoconv, ClamAV, Tesseract, Rar, Unrar, Unzip, 
+// / 7zipper, FFMPEG, PDFTOTEXT, Dia, PopplerUtils, MeshLab, Mkisofs & ImageMagick.
 // /
 // / <3 Open-Source
 // / -----------------------------------------------------------------------------------

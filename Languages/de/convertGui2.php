51ce961b1df037bae0ab5e0f893ec573d867a85f
Justin Grimes <zelon88@gmail.com>
2023-02-26 22:52:15 -0500

    v3.1.9.1 - Continue updating language packs.
    -v3.1.9.1.
    -Work on bn, de, en, ar, es, fr language packs.
    -Update language pack update procedure.
    -Update File Information section header text missing dots.
    -Evaluate subtitle conversion options.
      -https://ffmpeg.org/ffmpeg-all.html#Subtitle-Formats
      -https://superuser.com/questions/1090989/changing-subtitle-format-with-ffmpeg
      -WebVTT X X X X
      -SSA/ASS X X X X
      -SRT  X X X X
      -DVB X X X X
    -Add core support for subtitle conversions for testing purposes.
    -Added & documented ERROR 22000 for subtitle conversion errors.
    -Need to update the UI when this is ready.
    -Inspired by Issue # 32 (https://github.com/zelon88/HRConvert2/issues/32).

--- Languages/de/convertGui2.php
+++ Languages/de/convertGui2.php
@@ -278,7 +278,7 @@ if ($FileCount >= 2) $FCPlural1 = 'Sie haben '.$FileCount.' gültige Dateien auf
            onclick="toggle_visibility('videoOptionsDiv<?php echo $ConvertGuiCounter1; ?>'); toggle_visibility('videoButton<?php echo $ConvertGuiCounter1; ?>'); toggle_visibility('videoXButton<?php echo $ConvertGuiCounter1; ?>');"/>
           <?php } 
 
-          if (in_array($extension, $StreamArray) && $AllowStreams) { ?>
+          if (in_array($extension, $StreamArray) && in_array('Stream', $SupportedConversionType)) { ?>
           <a style="float:left;">&nbsp;|&nbsp;</a>
 
           <img id="streamButton<?php echo $ConvertGuiCounter1; ?>" name="streamButton<?php echo $ConvertGuiCounter1; ?>" src="Resources/stream.png" style="float:left; display:block;" 
@@ -545,7 +545,7 @@ if ($FileCount >= 2) $FCPlural1 = 'Sie haben '.$FileCount.' gültige Dateien auf
             <option value="iso">Iso</option>
             <option value="7z">7z</option>
           </select></p>
-          <input type="submit" id="archiveconvertSubmit<?php echo $ConvertGuiCounter1; ?>" name="archiveconvertSubmit<?php echo $ConvertGuiCounter1; ?>" value='Archiv Konvertieren' onclick="toggle_visibility('loadingCommandDiv<?php echo $ConvertGuiCounter1; ?>'); display:none;">
+          <input type="submit" id="archiveconvertSubmit<?php echo $ConvertGuiCounter1; ?>" name="archiveconvertSubmit<?php echo $ConvertGuiCounter1; ?>" value='Archiv Konvertieren' onclick="toggle_visibility('loadingCommandDiv<?php echo $ConvertGuiCounter1; ?>">
           <script type="text/javascript">
           $(document).ready(function () {
             $('#archiveconvertSubmit<?php echo $ConvertGuiCounter1; ?>').click(function() {
@@ -633,6 +633,7 @@ if ($FileCount >= 2) $FCPlural1 = 'Sie haben '.$FileCount.' gültige Dateien auf
             <option value="xls">Xls</option>
             <option value="xlsx">Xlsx</option>
             <option value="ods">Ods</option>
+            <option value="csv">Csv</option>
             <option value="pdf">Pdf</option>
           </select></p>
           <input type="submit" id="spreadconvertSubmit<?php echo $ConvertGuiCounter1; ?>" name="spreadconvertSubmit<?php echo $ConvertGuiCounter1; ?>" value='Tabellenkalkulation Konvertieren' onclick="toggle_visibility('loadingCommandDiv<?php echo $ConvertGuiCounter1; ?>');">        
@@ -809,7 +810,7 @@ if ($FileCount >= 2) $FCPlural1 = 'Sie haben '.$FileCount.' gültige Dateien auf
         </div>
         <?php } 
 
-        if (in_array($extension, $StreamArray) && $AllowStreams) {
+        if (in_array($extension, $StreamArray) && in_array('Stream', $SupportedConversionType)) {
         ?>
         <div id='streamOptionsDiv<?php echo $ConvertGuiCounter1; ?>' name='streamOptionsDiv<?php echo $ConvertGuiCounter1; ?>' style="max-width:750px; display:none;">
           <p style="max-width:1000px;"></p>

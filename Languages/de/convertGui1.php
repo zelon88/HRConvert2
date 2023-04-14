b5a0317e9cc680c84836b57ceedbd4774defbd57
Justin Grimes <zelon88@gmail.com>
2023-04-14 00:18:07 -0400

    v3.2.3 - Continue working on languageStrings concept, enable subtitle conversions.
    -v3.2.3.
    -Implemented subtitle conversions for Arabic, Bengali, German, & English language packs.
    -Work on German language pack.
      -Copy English as a template.
    -Language packs complete:
      -Ar l,h,1,2,f
      -Bn l,h,1,2,f
      -De l,h,1,2,f
      -En l,h,1,2,f
      -Es
      -Fr
      -Hi
      -It
      -Ko
      -Pt
      -Ru
      -Uk
      -Zh

--- Languages/de/convertGui1.php
+++ Languages/de/convertGui1.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 // / -----------------------------------------------------------------------------------
 // / APPLICATION INFORMATION ...
-// / HRConvert2, Copyright on 2/21/2023 by Justin Grimes, www.github.com/zelon88
+// / HRConvert2, Copyright on 4/13/2023 by Justin Grimes, www.github.com/zelon88
 // /
 // / LICENSE INFORMATION ...
 // / This project is protected by the GNU GPLv3 Open-Source license.
@@ -12,7 +12,7 @@
 // / on a server for users of any web browser without authentication.
 // /
 // / FILE INFORMATION ...
-// / v3.1.9.1.
+// / v3.2.3.
 // / This file contains language specific GUI elements for accepting file uploads.
 // /
 // / HARDWARE REQUIREMENTS ...
@@ -26,109 +26,125 @@
 // /
 // / <3 Open-Source
 // / -----------------------------------------------------------------------------------
-if (!isset($ApplicationName)) $ApplicationName = 'HRConvert2'; 
-if (!isset($ApplicationTitle)) $ApplicationTitle = 'Konvertieren Sie alles!'; 
-if (!isset($CoreLoaded)) die('ERROR!!! '.$ApplicationName.'-2, Diese Datei kann Ihre Anfrage nicht verarbeiten! Bitte senden Sie Ihre Datei stattdessen an convertCore.php!');
-if (!isset($ShowFinePrint)) $ShowFinePrint = TRUE;
+
+// / -----------------------------------------------------------------------------------
+// / Set a flag to tell that the UI has been displayed.
+$UIDisplayed = TRUE;
+// / Check if the core is loaded.
+if (!isset($CoreLoaded)) die('ERROR!!! HRConvert2-2, Diese Datei kann Ihre Anfrage nicht verarbeiten! Bitte senden Sie Ihre Datei stattdessen an convertCore.php!');
+// / Assign temporary variables.
+$gui2AudArr = $gui2VidArr = $gui2StreamArr = $gui2DocArr = $gui2SpreadArr = $gui2PresArr = $gui2ArchArr = $gui2ImaArr = $gui2ModArr = $gui2SubArr = $gui2DraArr = $gui2OcrArr = array();
+// / -----------------------------------------------------------------------------------
 ?>
   <body>
-    <?php 
+    <?php
     if (!isset($_GET['noGui'])) { ?>
-    <div id="header-text" style="max-width:1000px; margin-left:auto; margin-right:auto; text-align:center;">
+    <div id='header-text' style='max-width:1000px; margin-left:auto; margin-right:auto; text-align:center;'>
       <h1><?php echo $ApplicationName; ?></h1>
-      <h3>Online-Dateikonverter, Extraktor, Kompressor</h3>
+      <h3><?php echo $Gui1Text1; ?></h3>
       <hr />
     </div>
-    <div id="main" align="center">
-      <div id="overview" style="max-width:1000px; text-align:left; margin:25px;">
-      	<p id="info" style="display:block;"><?php echo $ApplicationName; ?> basiert auf der kostenlosen Anwendung <a href='https://github.com/zelon88/HRConvert2'>HRConvert2</a> von <a href='https://github.com/zelon88'>Zelon88</a>, die Dateien konvertiert, ohne Benutzer über das Internet zu verfolgen oder Ihr geistiges Eigentum zu verletzen.</p>
-        <button id="more-info-button" class="info-button" onclick="toggle_visibility('more-info'); toggle_visibility('more-info-button'); toggle_visibility('supported-formats-show-button'); 
-          toggle_visibility('less-info-button');" style="text-align:center; display:block; margin-left:auto; margin-right:auto;"><i>Mehr Informationen...</i></button>
-        <button id="less-info-button" class="info-button" onclick="toggle_visibility('more-info'); toggle_visibility('more-info-button'); toggle_visibility('supported-formats-show-button'); 
-          toggle_visibility('less-info-button');" style="text-align:center; display:none; margin-left:auto; margin-right:auto;"><i>Weniger Informationen...</i></button>
-        <div id="more-info" style="display:none;">
+    <div id='main' align='center'>
+      <div id='overview' style='max-width:1000px; text-align:<?php echo $GUIAlignment; ?>; margin:25px;'><?php echo $Gui1Text2; ?>
+        <p id='info' style='display:block;'></p>
+        <button id='more-info-button' class='info-button' onclick='toggle_visibility("more-info"); toggle_visibility("more-info-button"); toggle_visibility("supported-formats-show-button"); toggle_visibility("less-info-button");' style='text-align:center; display:block; margin-left:auto; margin-right:auto;'><?php echo $Gui1Text3; ?></button>
+        <button id='less-info-button' class='info-button' onclick='toggle_visibility("more-info"); toggle_visibility("more-info-button"); toggle_visibility("supported-formats-show-button"); toggle_visibility("less-info-button");' style='text-align:center; display:none; margin-left:auto; margin-right:auto;'><?php echo $Gui1Text4; ?></button>
+        <div id='more-info' style='display:none;'>
           <hr />
-          <p>Alle vom Benutzer bereitgestellten Daten werden automatisch gelöscht, sodass Sie sich keine Sorgen über den Verlust Ihrer persönlichen Daten oder Ihres Eigentums machen müssen, wenn Sie unsere Dienste nutzen.</p>
-          <p><?php echo $ApplicationName; ?> unterstützt 79 verschiedene Dateiformate, darunter Dokumente, Tabellenkalkulationen, Bilder, Medien, dreidimensionale Modelle, technische Zeichnungen, Vektordateien, Archive, Disk-Images und mehr.</p> 
-          <button id="supported-formats-show-button" class="info-button" onclick="toggle_visibility('supported-formats'); toggle_visibility('supported-formats-show-button'); 
-            toggle_visibility('supported-formats-hide-button');" style="text-align:center; display:none; margin-left:auto; margin-right:auto;"><i>Unterstützte Formate Anzeigen...</i></button>
-          <button id="supported-formats-hide-button" class="info-button" onclick="toggle_visibility('supported-formats'); toggle_visibility('supported-formats-show-button'); 
-            toggle_visibility('supported-formats-hide-button');" style="text-align:center; display:none; margin-left:auto; margin-right:auto;"><i>Unterstützte Formate Ausblenden...</i></button>
+          <p><?php echo $Gui1Text5; ?></p>
+          <p><?php echo $Gui1Text6; ?></p>
+          <button id='supported-formats-show-button' class='info-button' onclick='toggle_visibility("supported-formats"); toggle_visibility("supported-formats-show-button"); toggle_visibility("supported-formats-hide-button");' style='text-align:center; display:none; margin-left:auto; margin-right:auto;'><?php echo $Gui1Text7; ?></button>
+          <button id='supported-formats-hide-button' class='info-button' onclick='toggle_visibility("supported-formats"); toggle_visibility("supported-formats-show-button"); toggle_visibility("supported-formats-hide-button");' style='text-align:center; display:none; margin-left:auto; margin-right:auto;'><?php echo $Gui1Text8; ?></button>
           <br>
-          <div id="supported-formats" class="supported-formats" style="margin-left:33%; display:none;">
-            <h3>Unterstützte Formate</h3>
+          <div id='supported-formats' class='supported-formats' style='margin-left:33%; display:none;'>
+            <h3><?php echo $Gui1Text9; ?></h3>
             <hr />
-            <strong>Audioformate</strong>
-            <p><i>Unterstützt bestimmte Bitrate.</i></p>
+            <?php if (in_array('Audio', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text10; ?></strong>
+            <p><i><?php echo $Gui1Text11; ?></i></p>
             <ol>
-              <li>Mp2</li>
-              <li>Mp3</li>
-              <li>Avi</li>
-              <li>Flac</li>
-              <li>Ogg</li>
-              <li>Wav</li>
-              <li>Wma</li>
-              <li>M4a</li>
-              <li>M4p</li>
+              <?php foreach ($MediaArray as $gui1AudArr) { ?>
+              <li><?php echo $gui1AudArr; ?></li>
+              <?php } ?>
             </ol>
-            <strong>Videoformate</strong>
+            <?php } if (in_array('Video', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text12; ?></strong>
             <ol>
-              <li>3gp</li>
-              <li>Mkv</li>
-              <li>Avi</li>
-              <li>Mp4</li>
-              <li>Flv</li>
-              <li>Mpeg</li>
-              <li>Wmv</li>
-              <li>Mov</li>
-              <li>M4v</li>
+              <?php foreach ($VideoArray as $gui1VidArr) { ?>
+              <li><?php echo $gui1VidArr; ?></li>
+              <?php } ?>
             </ol>
-            <strong>Stream-Formate</strong>
+            <?php } if (in_array('Stream', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text13; ?></strong>
+            <p><i><?php echo $Gui1Text30; ?></i></p>
             <ol>
-              <li>M3u8</li>
+              <?php foreach ($StreamArray as $gui1StreamArr) { ?>
+              <li><?php echo $gui1StreamArr; ?></li>
+              <?php } ?>
             </ol>
-            <strong>Dokumentformate</strong>
+            <?php } if (in_array('Document', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text14; ?></strong>
             <ol>
-              <li>Doc</li>
-              <li>Docx</li>
-              <li>Txt</li>
-              <li>Rtf</li>
-              <li>Odt</li>
-              <li>Pdf</li>
+              <?php foreach ($DocumentArray as $gui1DocArr) { ?>
+              <li><?php echo $gui1DocArr; ?></li>
+              <?php } ?>
             </ol>
-            <strong>Tabellenformate</strong>
+            <?php } if (in_array('Document', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text15; ?></strong>
             <ol>
-              <li>Xls</li>
-              <li>Xlsx</li>
-              <li>Ods</li>
-              <li>Csv</li>
-              <li>Pdf</li>
+              <?php foreach ($SpreadsheetArray as $gui1SpreadArr) { ?>
+              <li><?php echo $gui1SpreadArr; ?></li>
+              <?php } ?>
             </ol>
-            <strong>Präsentationsformate</strong>
+            <?php } if (in_array('Document', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text16; ?></strong>
             <ol>
-              <li>Pages</li>
-              <li>Pptx</li>
-              <li>Ppt</li>
-              <li>Xps</li>
-              <li>Pot</li>
-              <li>Potx</li>
-              <li>Ppa</li>
-              <li>Ppt</li>
-              <li>Pptx</li>
-              <li>Odp</li>
+              <?php foreach ($PresentationArray as $gui1PresArr) { ?>
+              <li><?php echo $gui1PresArr; ?></li>
+              <?php } ?>
             </ol>
-            <strong>Archivformate</strong>
-            <p><i>Kann zwischen Archivformaten und Disk-Image-Formaten konvertieren.</i></p>
+            <?php } if (in_array('Archive', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text17; ?></strong>
+            <p><i><?php echo $Gui1Text18; ?></i></p>
             <ol>
-              <li>Zip</li>
-              <li>Rar</li>
-              <li>Tar</li>
-              <li>7z</li>
-              <li>Iso</li>
+              <?php foreach ($DearchiveArray as $gui1ArchArr) { ?>
+              <li><?php echo $gui1ArchArr; ?></li>
+              <?php } ?>
             </ol>
-            <strong>Bildformate</strong>
-            <p><i>Kann Bilder von Dokumenten in Dokumentformate konvertieren.</i></p>
-            <p><i>Unterstützt die Größenänderung und Drehung.</i></p>
+            <?php } if (in_array('Image', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text19; ?></strong>
+            <p><i><?php echo $Gui1Text21; ?></i></p>
+            <ol>
+              <?php foreach ($ImageArray as $gui1ImaArr) { ?>
+              <li><?php echo $gui1ImaArr; ?></li>
+              <?php } ?>
+            </ol>
+            <?php } if (in_array('Model', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text22; ?></strong>
+            <ol>
+              <?php foreach ($ModelArray as $gui1ModArr) { ?>
+              <li><?php echo $gui1ModArr; ?></li>
+              <?php } ?>
+            </ol>
+            <?php } if (in_array('Subtitle', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text31; ?></strong>
+            <ol>
+              <?php foreach ($SubtitleArray as $gui1SubArr) { ?>
+              <li><?php echo $gui1SubArr; ?></li>
+              <?php } ?>
+            </ol>
+            <?php } if (in_array('Drawing', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text23; ?></strong>
+            <p><i><?php echo $Gui1Text24; ?></i></p>
+            <ol>
+              <?php foreach ($DrawingArray as $gui1DraArr) { ?>
+              <li><?php echo $gui1DraArr; ?></li>
+              <?php } ?>
+            </ol>
+            <?php } if (in_array('OCR', $SupportedConversionTypes)) { ?>
+            <strong><?php echo $Gui1Text25; ?></strong>
+            <p><i><?php echo $Gui1Text20; ?></i></p>
+            <p><i><?php echo $Gui1Text26; ?></i></p>
             <ol>
               <li>Jpg</li>
               <li>Jpeg</li>
@@ -137,52 +153,8 @@ if (!isset($ShowFinePrint)) $ShowFinePrint = TRUE;
               <li>Pdf</li>
               <li>Gif</li>
               <li>Webp</li>
-              <li>Cin</li>
-              <li>Dds</li>
-              <li>Dib</li>
-              <li>Flif</li>
-              <li>Avif</li>
-              <li>Gplt</li>
-              <li>Sct</li>
-              <li>Xcf</li>
-              <li>Heic</li>
-              <li>Ico</li>
-            </ol>
-            <strong>Dreidimensionale Modellformate</strong>
-            <ol>
-              <li>3ds</li>
-              <li>Obj</li>
-              <li>Collada</li>
-              <li>Off</li>
-              <li>Ply</li>
-              <li>Stl</li>
-              <li>Gts</li>
-              <li>Dxf</li>
-              <li>U3d</li>
-              <li>X3d</li>
-              <li>Vrml</li>
-            </ol>
-            <strong>Zeichnungsformate</strong>
-            <p><i>Kann Zeichnungsdateien in Bildformate konvertieren.</i></p>
-            <ol>
-              <li>Svg</li>
-              <li>Dxf</li>
-              <li>Fig</li>
-              <li>Vdx</li>
-              <li>Dia</li>
-              <li>Wpg</li>
-            </ol>
-            <strong>Unterstützung der optischen Zeichenerkennung</strong>
-            <p><i>Optische Zeichenerkennungsoperationen unterstützen die folgenden Eingabeformate...</i></p>
-            <ol>
-              <li>Jpg</li>
-              <li>Jpeg</li>
-              <li>Png</li>
-              <li>Bmp</li>
-              <li>Pdf</li>
-              <li>Gif</li>
               </ol>
-            <p><i>Optische Zeichenerkennungsoperationen unterstützen die folgenden Ausgabeformate...</i></p>
+            <p><i><?php echo $Gui1Text27; ?></i></p>
             <ol>
               <li>Doc</li>
               <li>Docx</li>
@@ -191,30 +163,31 @@ if (!isset($ShowFinePrint)) $ShowFinePrint = TRUE;
               <li>Odt</li>
               <li>Pdf</li>
             </ol>
+            <?php } ?>
           </div>
         </div>
         <hr />
       </div>
       <?php } ?>
-      <div align="center">
-        <div id="call-to-action1" style="max-width:1000px; text-align:center;">
-          <p>Wählen Sie Dateien aus, indem Sie sie anklicken, antippen oder in das unten stehende Feld ziehen.</p>
+      <div align='center'>
+        <div id='call-to-action1' title='' style='max-width:1000px; text-align:center;'>
+          <p><?php echo $Gui1Text28; ?></p>
         </div>
       </div>
-      <div align="center">
-        <div id="dropzone" style="max-height:1000px; max-width:1000px; margin:25px;">
-          <form action="convertCore.php" class="dropzone" id="filesToUpload" name="filesToUpload" method="post" enctype="multipart/form-data">
-          <input type="hidden" id="token1" name="Token1" value="<?php echo $Token1; ?>">
-          <input type="hidden" id="token2" name="Token2" value="<?php echo $Token2; ?>">
+      <div align='center'>
+        <div id='dropzone' style='max-height:1000px; max-width:1000px; margin:25px;'>
+          <form action='convertCore.php' class='dropzone' id='filesToUpload' name='filesToUpload' method='post' enctype='multipart/form-data'>
+          <input type='hidden' id='token1' name='Token1' value='<?php echo $Token1; ?>'>
+          <input type='hidden' id='token2' name='Token2' value='<?php echo $Token2; ?>'>
           </form>
         </div>
       </div>
-      <div align="center">
-        <div id="continue" style="max-width:1000px; text-align:center;">
-          <form action="convertCore.php?showFiles=1<?php if (isset($_GET['noGui'])) echo '&noGui=TRUE'; if (isset($_GET['language'])) echo '&language='.$_GET['language']; ?>" method="post">
-            <input type="hidden" id="token1" name="Token1" value="<?php echo $Token1; ?>">
-            <input type="hidden" id="token2" name="Token2" value="<?php echo $Token2; ?>">
-            <input type="submit" id="continue-button" class="info-button" value="Fortsetzen...">
+      <div align='center'>
+        <div id='continue' style='max-width:1000px; text-align:center;'>
+          <form action='convertCore.php?showFiles=1<?php if (isset($_GET['noGui'])) echo '&noGui=TRUE'; if (isset($_GET['language'])) echo '&language='.$_GET['language']; ?>' method='post'>
+            <input type='hidden' id='token1' name='Token1' value='<?php echo $Token1; ?>'>
+            <input type='hidden' id='token2' name='Token2' value='<?php echo $Token2; ?>'>
+            <input type='submit' id='continue-button' class='info-button' value='<?php echo $Gui1Text29; ?>'>
           </form>
           <br />
           <?php if (!isset($_GET['noGui'])) { ?>
@@ -225,4 +198,7 @@ if (!isset($ShowFinePrint)) $ShowFinePrint = TRUE;
 
     <?php if (!isset($_GET['noGui'])) { ?>
     </div>
-    <?php } ?>
+    <?php }
+    // / Manually clean up sensitive memory. Helps to keep track of variable assignments.
+    $gui1AudArr = $gui1VidArr = $gui1StreamArr = $gui1DocArr = $gui1SpreadArr = $gui1PresArr = $gui1ArchArr = $gui1ImaArr = $gui1ModArr = $gui2SubArr = $gui1DraArr = NULL;
+    unset($gui1AudArr, $gui1VidArr, $gui1StreamArr, $gui1DocArr, $gui1SpreadArr, $gui1PresArr, $gui1ArchArr, $gui1ImaArr, $gui1ModArr, $gui2SubArr, $gui1DraArr);
--- Languages/de/footer.php
+++ Languages/de/footer.php
@@ -1,7 +1,7 @@
-    <?php 
+    <?php
     // / -----------------------------------------------------------------------------------
     // / APPLICATION INFORMATION ...
-    // / HRConvert2, Copyright on 2/21/2023 by Justin Grimes, www.github.com/zelon88
+    // / HRConvert2, Copyright on 4/13/2023 by Justin Grimes, www.github.com/zelon88
     // /
     // / LICENSE INFORMATION ...
     // / This project is protected by the GNU GPLv3 Open-Source license.
@@ -12,7 +12,7 @@
     // / on a server for users of any web browser without authentication.
     // /
     // / FILE INFORMATION ...
-    // / v3.1.9.1.
+    // / v3.2.3.
     // / This file contains language specific GUI elements to be displayed at the bottom of pages.
     // /
     // / HARDWARE REQUIREMENTS ...
@@ -26,11 +26,17 @@
     // /
     // / <3 Open-Source
     // / ----------------------------------------------------------------------------------- 
-    if (!isset($ShowFinePrint)) $ShowFinePrint = TRUE;
+
+    // / -----------------------------------------------------------------------------------
+    // / Set a flag to tell that the UI has been displayed.
+    $FooterDisplayed = TRUE;
+    // / Check if the core is loaded.
+    if (!isset($CoreLoaded)) die('ERROR!!! HRConvert2-2, Diese Datei kann Ihre Anfrage nicht verarbeiten! Bitte senden Sie Ihre Datei stattdessen an convertCore.php!');
+    // / -----------------------------------------------------------------------------------
+
     if ($ShowFinePrint) { ?>
-    <div id="footer" name="footer" style="max-width:1000px; margin-left:auto; margin-right:auto; text-align:center;">
-      <p>Lesen Sie unsere <a href="<?php echo $TOSURL; ?>" target="_blank" rel="noopener noreferrer">Nutzungsbedingungen</a>
-       und <a href="<?php echo $PPURL; ?>" target="_blank" rel="noopener noreferrer">Datenschutzrichtlinie</a></p>
+    <div id='footer' name='footer' style='max-width:1000px; margin-left:auto; margin-right:auto; text-align:center;'>
+      <p><?php echo $GuiFooterText1; ?></a></p>
     </div>
   <?php } ?>
 

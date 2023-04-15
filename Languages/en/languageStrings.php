cb25d4e482773a53f0e80840f5c35796a0a388d2
Justin Grimes <zelon88@gmail.com>
2023-04-15 01:03:52 -0400

    v3.2.4 - Misc cleanup.
    -v3.2.4.
    -Merge security related checks in file operations inside the error check code.
      -Try to make them all consistent. Move some log code to after the sanitization process in upload/download/delete.
    -The log message 'Could not verify tokens!' will now only appear in logs if $Verbose is set in config.php.
    -Fix missing translations in subtitles, subtitles labeled as models.
      -Was leftover copy-pasta.
    -Add jpe to supported formats.
    -I really want to rework PDFs soon. 
      -Want to add the ability to convert between any supported format using existing functions.

--- Languages/en/languageStrings.php
+++ Languages/en/languageStrings.php
@@ -1,7 +1,7 @@
 <?php
 // / -----------------------------------------------------------------------------------
 // / APPLICATION INFORMATION ...
-// / HRConvert2, Copyright on 4/13/2023 by Justin Grimes, www.github.com/zelon88
+// / HRConvert2, Copyright on 4/14/2023 by Justin Grimes, www.github.com/zelon88
 // /
 // / LICENSE INFORMATION ...
 // / This project is protected by the GNU GPLv3 Open-Source license.
@@ -12,7 +12,7 @@
 // / on a server for users of any web browser without authentication.
 // /
 // / FILE INFORMATION ...
-// / v3.2.3.
+// / v3.2.4.
 // / This file contains language specific GUI related text for performing file conversions.
 // /
 // / HARDWARE REQUIREMENTS ...
@@ -276,6 +276,10 @@ $Gui2Text72 = 'Cannot perform a virus scan on this file!';
 $Gui2Text73 = 'File Link Copied to Clipboard!';
 // / 'Operation Failed!'
 $Gui2Text74 = 'Operation Failed!';
+// / Convert These Subtitles'
+$Gui2Text75 = 'Convert These Subtitles';
+// / Convert Subtitles'
+$Gui2Text76 = 'Convert Subtitles';
 // / -----------------------------------------------------------------------------------
 
 // / -----------------------------------------------------------------------------------

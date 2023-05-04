a0387ec31b29c3ed1ad1b60dc6bd5fcd7e4c55ce
Justin Grimes <zelon88@gmail.com>
2023-05-04 01:06:44 -0400

    v3.2.8 - Continue implementing separation of UI from language packs.
    -v3.2.8.
    -A UI now contains the language packs for the UI.
    -A UI now contains it's own styleCore.php file containing stylesheets for each color.
    -The UI is now completely modular!
    -Multiple UIs can now be installed simultaniously and selected by the user via GET request.
      -Adding supported GUIs is the same process as adding supported language packs.
    -Add "CREATING_GUIS.txt" file to 'Documentation' folder.
    -update "CREATING_LANGUAGE_PACKS.txt" to reflect the new locations & GUI / language pack format.
    -Add languageStrings.php to Italian language pack.
      -Language packs complete:
      -Note that h,1,2,f are not needed anymore because they are all the same, stored in \UI folder.
        -Ar l
        -Bn l
        -De l
        -En l
        -Es l
        -Fr l
        -Hi l
        -It l
        -Ko
        -Pt
        -Ru
        -Uk
        -Zh

---
+++ UI/Default/Languages/it/convertGui1.php
@@ -0,0 +1,228 @@
+<?php 
+// / -----------------------------------------------------------------------------------
+// / APPLICATION INFORMATION ...
+// / HRConvert2, Copyright on 2/21/2023 by Justin Grimes, www.github.com/zelon88
+// /
+// / LICENSE INFORMATION ...
+// / This project is protected by the GNU GPLv3 Open-Source license.
+// / https://www.gnu.org/licenses/gpl-3.0.html
+// /
+// / APPLICATION INFORMATION ...
+// / This application is designed to provide a web-interface for converting file formats
+// / on a server for users of any web browser without authentication.
+// /
+// / FILE INFORMATION ...
+// / v3.1.9.1.
+// / This file contains language specific GUI elements for accepting file uploads.
+// /
+// / HARDWARE REQUIREMENTS ...
+// / This application requires at least a Raspberry Pi Model B+ or greater.
+// / This application will run on just about any x86 or x64 computer.
+// /
+// / DEPENDENCY REQUIREMENTS ...
+// / This application requires Debian Linux (w/3rd Party audio license),
+// / Apache 2.4, PHP 8+, LibreOffice, Unoconv, ClamAV, Tesseract, Rar, Unrar, Unzip,
+// / 7zipper, FFMPEG, PDFTOTEXT, Dia, PopplerUtils, MeshLab, Mkisofs & ImageMagick.
+// /
+// / <3 Open-Source
+// / -----------------------------------------------------------------------------------
+if (!isset($ApplicationName)) $ApplicationName = 'HRConvert2'; 
+if (!isset($ApplicationTitle)) $ApplicationTitle = 'Converti Qualsiasi Cosa!'; 
+if (!isset($CoreLoaded)) die('ERRORE!!! '.$ApplicationName.'-2, questo file non può elaborare la tua richiesta! Invia invece il tuo file a convertCore.php!');
+if (!isset($ShowFinePrint)) $ShowFinePrint = TRUE;
+?>
+  <body>
+    <?php 
+    if (!isset($_GET['noGui'])) { ?>
+    <div id="header-text" style="max-width:1000px; margin-left:auto; margin-right:auto; text-align:center;">
+      <h1><?php echo $ApplicationName; ?></h1>
+      <h3>Online File Converter, Extractor, Compressor</h3>
+      <hr />
+    </div>
+    <div id="main" align="center">
+      <div id="overview" style="max-width:1000px; text-align:left; margin:25px;">
+      	<p id="info" style="display:block;"><?php echo $ApplicationName; ?> è basato sull'applicazione open source <a href='https://github.com/zelon88/HRConvert2'>HRConvert2</a> di <a href='https://github.com/zelon88'>Zelon88</a>. È progettato per convertire file senza tracciare gli utenti o violare la tua proprietà intellettuale.</p>
+        <button id="more-info-button" class="info-button" onclick="toggle_visibility('more-info'); toggle_visibility('more-info-button'); toggle_visibility('supported-formats-show-button'); 
+          toggle_visibility('less-info-button');" style="text-align:center; display:block; margin-left:auto; margin-right:auto;"><i>Maggiori Informazioni ...</i></button>
+        <button id="less-info-button" class="info-button" onclick="toggle_visibility('more-info'); toggle_visibility('more-info-button'); toggle_visibility('supported-formats-show-button'); 
+          toggle_visibility('less-info-button');" style="text-align:center; display:none; margin-left:auto; margin-right:auto;"><i>Meno Informazioni ...</i></button>
+        <div id="more-info" style="display:none;">
+          <hr />
+          <p>Tutti i dati forniti dall'utente vengono cancellati automaticamente, quindi non devi preoccuparti di perdere le tue informazioni personali o la tua proprietà durante l'utilizzo dei nostri servizi.</p>
+          <p>Attualmente <?php echo $ApplicationName; ?> supporta 79 formati di file diversi, inclusi documenti, fogli di calcolo, immagini, media, modelli tridimensionali, disegni tecnici, file vettoriali, archivi, immagini disco e altro.</p> 
+          <button id="supported-formats-show-button" class="info-button" onclick="toggle_visibility('supported-formats'); toggle_visibility('supported-formats-show-button'); 
+            toggle_visibility('supported-formats-hide-button');" style="text-align:center; display:none; margin-left:auto; margin-right:auto;"><i>Visualizza i Formati Supportati ...</i></button>
+          <button id="supported-formats-hide-button" class="info-button" onclick="toggle_visibility('supported-formats'); toggle_visibility('supported-formats-show-button'); 
+            toggle_visibility('supported-formats-hide-button');" style="text-align:center; display:none; margin-left:auto; margin-right:auto;"><i>Nascondi Formati Supportati ...</i></button>
+          <br>
+          <div id="supported-formats" class="supported-formats" style="margin-left:33%; display:none;">
+            <h3>Formati Supportati</h3>
+            <hr />
+            <strong>Formati di Audio</strong>
+            <p><i>Supporta bitrate specifici.</i></p>
+            <ol>
+              <li>Mp2</li>
+              <li>Mp3</li>
+              <li>Avi</li>
+              <li>Flac</li>
+              <li>Ogg</li>
+              <li>Wav</li>
+              <li>Wma</li>
+              <li>M4a</li>
+              <li>M4p</li>
+            </ol>
+            <strong>Formati di Video</strong>
+            <ol>
+              <li>3gp</li>
+              <li>Mkv</li>
+              <li>Avi</li>
+              <li>Mp4</li>
+              <li>Flv</li>
+              <li>Mpeg</li>
+              <li>Wmv</li>
+              <li>Mov</li>
+              <li>M4v</li>
+            </ol>
+            <strong>Formati di Streaming</strong>
+            <ol>
+              <li>M3u8</li>
+            </ol>
+            <strong>Formati di Documenti</strong>
+            <ol>
+              <li>Doc</li>
+              <li>Docx</li>
+              <li>Txt</li>
+              <li>Rtf</li>
+              <li>Odt</li>
+              <li>Pdf</li>
+            </ol>
+            <strong>Formati di Fogli di Calcolo</strong>
+            <ol>
+              <li>Xls</li>
+              <li>Xlsx</li>
+              <li>Ods</li>
+              <li>Csv</li>
+              <li>Pdf</li>
+            </ol>
+            <strong>Formati di Presentazione</strong>
+            <ol>
+              <li>Pages</li>
+              <li>Pptx</li>
+              <li>Ppt</li>
+              <li>Xps</li>
+              <li>Pot</li>
+              <li>Potx</li>
+              <li>Ppa</li>
+              <li>Ppt</li>
+              <li>Pptx</li>
+              <li>Odp</li>
+            </ol>
+            <strong>Formati di Archivio</strong>
+            <p><i>Può convertire tra formati di archivio e formati di immagini disco.</i></p>
+            <ol>
+              <li>Zip</li>
+              <li>Rar</li>
+              <li>Tar</li>
+              <li>7z</li>
+              <li>Iso</li>
+            </ol>
+            <strong>Formati di Immagini</strong>
+            <p><i>Può convertire immagini di documenti in formati di documenti.</i></p>
+            <p><i>Supporta il ridimensionamento e la rotazione.</i></p>
+            <ol>
+              <li>Jpg</li>
+              <li>Jpeg</li>
+              <li>Png</li>
+              <li>Bmp</li>
+              <li>Pdf</li>
+              <li>Gif</li>
+              <li>Webp</li>
+              <li>Cin</li>
+              <li>Dds</li>
+              <li>Dib</li>
+              <li>Flif</li>
+              <li>Avif</li>
+              <li>Gplt</li>
+              <li>Sct</li>
+              <li>Xcf</li>
+              <li>Heic</li>
+              <li>Ico</li>
+            </ol>
+            <strong>Formati di Modelli Tridimensionali</strong>
+            <ol>
+              <li>3ds</li>
+              <li>Obj</li>
+              <li>Collada</li>
+              <li>Off</li>
+              <li>Ply</li>
+              <li>Stl</li>
+              <li>Gts</li>
+              <li>Dxf</li>
+              <li>U3d</li>
+              <li>X3d</li>
+              <li>Vrml</li>
+            </ol>
+            <strong>Formati di Disegno</strong>
+            <p><i>Può convertire file di disegno in formati immagine.</i></p>
+            <ol>
+              <li>Svg</li>
+              <li>Dxf</li>
+              <li>Fig</li>
+              <li>Vdx</li>
+              <li>Dia</li>
+              <li>Wpg</li>
+            </ol>
+            <strong>Supporto al Riconoscimento Ottico dei Caratteri</strong>
+            <p><i>Le operazioni di riconoscimento ottico dei caratteri supportano i seguenti formati di input...</i></p>
+            <ol>
+              <li>Jpg</li>
+              <li>Jpeg</li>
+              <li>Png</li>
+              <li>Bmp</li>
+              <li>Pdf</li>
+              <li>Gif</li>
+              </ol>
+            <p><i>Le operazioni di riconoscimento ottico dei caratteri supportano i seguenti formati di output...</i></p>
+            <ol>
+              <li>Doc</li>
+              <li>Docx</li>
+              <li>Txt</li>
+              <li>Rtf</li>
+              <li>Odt</li>
+              <li>Pdf</li>
+            </ol>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <?php } ?>
+      <div align="center">
+        <div id="call-to-action1" style="max-width:1000px; text-align:center;">
+          <p>Seleziona i file facendo clic, toccando o rilasciando i file nella casella sottostante.</p>
+        </div>
+      </div>
+      <div align="center">
+        <div id="dropzone" style="max-height:1000px; max-width:1000px; margin:25px;">
+          <form action="convertCore.php" class="dropzone" id="filesToUpload" name="filesToUpload" method="post" enctype="multipart/form-data">
+          <input type="hidden" id="token1" name="Token1" value="<?php echo $Token1; ?>">
+          <input type="hidden" id="token2" name="Token2" value="<?php echo $Token2; ?>">
+          </form>
+        </div>
+      </div>
+      <div align="center">
+        <div id="continue" style="max-width:1000px; text-align:center;">
+          <form action="convertCore.php?showFiles=1<?php if (isset($_GET['noGui'])) echo '&noGui=TRUE'; if (isset($_GET['language'])) echo '&language='.$_GET['language']; ?>" method="post">
+            <input type="hidden" id="token1" name="Token1" value="<?php echo $Token1; ?>">
+            <input type="hidden" id="token2" name="Token2" value="<?php echo $Token2; ?>">
+            <input type="submit" id="continue-button" class="info-button" value="Continua ...">
+          </form>
+          <br />
+          <?php if (!isset($_GET['noGui'])) { ?>
+          <hr />
+          <?php } ?>
+        </div>
+      </div>
+
+    <?php if (!isset($_GET['noGui'])) { ?>
+    </div>
+    <?php } ?>

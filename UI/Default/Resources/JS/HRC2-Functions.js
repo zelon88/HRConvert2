826574ed654ba70c2e8dfa0a14518857e884329d
Justin Grimes <zelon88@gmail.com>
2024-03-07 00:43:29 -0500

    v3.3.1 - Reorganize folders, create docker file, consolidate UI.
    -v3.3.1.
    -Move a bunch of files.
      -Move versionInfo.php and config.php into the Resources folder.
      -Create build folder.
      -Move rc.local, rc.local.service, and a php.ini sample file in there that will be used for Docker installs.
      -Move all resources into the UI\Default folder.
      -Relieve the core of the logic of defining CSS, JS, and images for the UI pack. That's not very modular.
      -Now the UI pack defines these in header.php.
      -Resources folder is not a required part of a UI pack. So the core won't check for it, 
      -Although the core does define a relative path where the Resources folder, and JS, CSS, Images subfolders should go as a convinience.
      -It is not required by the core to have these folders, or to organize your theme into them.
      -Remove LICENSE file from the root of the repository, as this is also in the Documentation folder. 
    -Add a return value to cleanFiles() because I need one for the changes to verifyInstallation().
      -Use the return value to make the variables $CleanedConvertLoc and $CleanedTempLoc meaningful by passing the return value to them.
    -verifyInstallation() now checks config.php for $DeleteBuildEnvironment, and if true will delete the /Build folder.
      -This is enabled by default to prevent attackers from using this to enumerate configuration information about the server or application.
    -Improve documentation, formatting of styleCore.php for consistency/readability.
    -Continue the long, ardous task of converting all the double quotes in convertGui2.php to single quotes.
      -This will take a while, but I have done enough to be able to find/replace what I need.
    -Remove Error 2-1 from ERROR_DESCRIPTIONS.txt because it is no longer generated by the application.
    -Move supported formats to config.php so server administrators can control their own fate, if they dare.
      -Default values are also listed in config.php if needed.
    -Add support to remove the build environment & "development docs" that give away version info like readme.md & changelog.txt.
    -Changed default color scheme from grey to blue because grey is especially gross looking to me right now.
    -Fix bug where font defined in config.php was not propagating to styleCore.php.
    -Fix bug with user virus scans when using ClamAV to scan an individual file for viruses. 
      -The scan would complete, but it would utilize both ScanCore and ClamAV when all the user asked for was ClamAV.
    -Move default installation location to /DATA/HRConvert2 to make it consistent for Docker implementation.
    -Start working on official Docker version.
    -Minor update to installation instructions, consistency, consolidate steps.
    -Added icon files to the Resources directory to be used in a future update.

---
+++ UI/Default/Resources/JS/HRC2-Functions.js
@@ -0,0 +1,49 @@
+// / -----------------------------------------------------------------------------------
+// / APPLICATION INFORMATION ...
+// / HRConvert2, Copyright on 4/11/2022 by Justin Grimes, www.github.com/zelon88
+// /
+// / LICENSE INFORMATION ...
+// / This project is protected by the GNU GPLv3 Open-Source license.
+// / https://www.gnu.org/licenses/gpl-3.0.html
+// /
+// / APPLICATION INFORMATION ...
+// / This application is designed to provide a web-interface for converting file formats
+// / on a server for users of any web browser without authentication.
+// /
+// / FILE INFORMATION
+// / This file contains the client side javascript library that supports the HRConvert2 GUI.
+// /
+// / HARDWARE REQUIREMENTS ...
+// / This application requires at least a Raspberry Pi Model B+ or greater.
+// / This application will run on just about any x86 or x64 computer.
+// /
+// / DEPENDENCY REQUIREMENTS ...
+// / This application requires Debian Linux (w/3rd Party audio license),
+// / Apache 2.4, PHP 8+, LibreOffice, Unoconv, ClamAV, Tesseract, Rar, Unrar, Unzip,
+// / 7zipper, FFMPEG, PDFTOTEXT, Dia, PopplerUtils, MeshLab, Mkisofs & ImageMagick.
+// /
+// / <3 Open-Source
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / A function to toggle the visibility of an HTML element.
+// / Switches the 'Visibility' property between 'block' & 'none'.
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if (e.style.display == 'block') e.style.display = 'none';
+  else e.style.display = 'block'; }
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / A function to trigger the browsers back functionality when called.
+function goBack() {
+  window.history.back(); }
+// / -----------------------------------------------------------------------------------
+
+// / -----------------------------------------------------------------------------------
+// / A function to copy an input URL to a users clipboard and output an alert in the local language.
+function copy_share_link(url) {
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(url); }
+  else alert('Your brower does not support copying to the clipboard!'); }
+// / -----------------------------------------------------------------------------------

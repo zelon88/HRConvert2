45c7f886ad71239724801ff0ad9afb86d280bb41
Justin Grimes <zelon88@gmail.com>
2026-05-05 03:43:06 -0400

    v3.4 - Strengthen sanitization process. Improve OCR, Document Conversions.
    -v3.4.
    -Further strengthen the sanitization process to prevent potential command injection.
      -Github user KhaelK138 discovered another sanitization vulnerability, this one as severe as the last one.
      -Added chr(10) to the lists of sanitized characters in both strict and non-strict modes.
      -GHSA-f74g-4wj8-j35h
    -Improve OCR operations.
      -More verbose logging, when Verbose is enabled in config.php.
      -Minor corrections to code spacing and grouping.
      -Added Error 15010 "Could not verify the document conversion engine...".
      -Added Error 15011 "Could not create a file at ..." for when OCR operations fail to generate required files.
      -Fixed a bug where the error checker was nested too high and would only really check the last operation performed by the loop.
        -Typically only one file operation is performed at a time with the standard GUI, but the core can do batches of files. Well, now it can ;).
    -Discovered that document conversions won't work if soffice.bin is running as another user.
      -Added checks and a mitigation for this in the verifyDocumentEngine() function.
      -Previously the verifyDocumentEngine() function would not check who the owner was.
      -Now verifyDocumentEngine() function will check who the owner is, and if it is not root or $ApacheUser will attempt to start a new instance.
    -Improve whitespace consistency.
      -Remove whitespace chars after opening brackets.
    -Improve comment consistency.
      -Make sure that when code snippits appear multiple times that the comments sections match.
    -Add several new comment sections, elaborate on several others.
    -Remove unused $returnData variable references in the userVirusScan() function.
    -Updated config.php.
      -Added $HomeLoc variable for the $ApacheUser home folder.
      -Added $DocumentEngineSleepTimer to determine how long to wait after trying to start the Document Conversion Engine.
    -Added a line of code to the verifyGlobals() function to specify the home folder for the $ApacheUser to be the $HomeLoc.

--- Resources/versionInfo.php
+++ Resources/versionInfo.php
@@ -1,7 +1,7 @@
 <?php
 // / -----------------------------------------------------------------------------------
 // / COPYRIGHT INFORMATION ...
-// / HRConvert2, Copyright on 5/3/2026 by Justin Grimes, www.github.com/zelon88
+// / HRConvert2, Copyright on 5/5/2026 by Justin Grimes, www.github.com/zelon88
 // /
 // / LICENSE INFORMATION ...
 // / This project is protected by the GNU GPLv3 Open-Source license.
@@ -12,7 +12,7 @@
 // / on a server for users of any web browser without authentication.l
 // /
 // / FILE INFORMATION ...
-// / v3.3.8.
+// / v3.4.
 // / This file contains the current HRConvert2 version for update verification purposes.
 // /
 // / HARDWARE REQUIREMENTS ...
@@ -30,5 +30,5 @@
 
 // / -----------------------------------------------------------------------------------
 // / The version of this HRConvert2 installation.
-$Version = 'v3.3.8';
+$Version = 'v3.4';
 // / -----------------------------------------------------------------------------------

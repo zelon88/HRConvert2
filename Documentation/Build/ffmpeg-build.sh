4b91184bd232e4a4a7d4feabacd96ed8d246c9aa
Justin Grimes <zelon88@gmail.com>
2026-05-18 12:48:31 -0400

    v3.4.5 - Preparing for Docker update.
    -v3.4.5.
    -Working dilligently on the Docker build process update.
    -Added entrypoint.sh to contain runtime code.
    -Take over maintanence of Unoconv.
      -Now HRConvert2 bundles it's own fork of Unoconv.
      -Add option called $UsePatchedDocumentEngine to config.php to specify which Unoconv to use.
    -Update convertCore.php to specifically call unoconv with verbose and profile path arguments.
      -Set profile path to $HomeLoc which should be /var/www.
      -Explicitly define which Unoconv to use.
    -Update docker build process.
      -Adjust dependencies to optimize compatibility.
      -Update url in ffmpeg build scripts.
      -Add file headers to ffmpeg build scripts.
      -Update Docker PHP version to php 8.5.

--- Documentation/Build/ffmpeg-build.sh
+++ Documentation/Build/ffmpeg-build.sh
@@ -1,4 +1,33 @@
 #!/bin/bash
+
+# -----------------------------------------------------------------------------------
+# COPYRIGHT INFORMATION ...
+# HRConvert2, Copyright on 5/18/2026 by Justin Grimes, www.github.com/zelon88
+
+# LICENSE INFORMATION ...
+# This project is protected by the GNU GPLv3 Open-Source license.
+# https://www.gnu.org/licenses/gpl-3.0.html
+
+# APPLICATION INFORMATION ...
+# This application is designed to provide a web-interface for converting file formats
+# on a server for users of any web browser without authentication.
+
+# FILE INFORMATION ...
+# v3.4.5.
+# This file helps build FFMPEG from source to enable more advanced file operations.
+
+# HARDWARE REQUIREMENTS ...
+# This application requires at least a Raspberry Pi Model B+ or greater.
+# This application will run on just about any x86 or x64 computer.
+
+# DEPENDENCY REQUIREMENTS ...
+# This application requires Debian Linux, Apache 2.4, PHP 8+, FFMPEG, Dia,
+# Mkisofs, 7zip, LibreOffice, Unoconv, libgxps-utils, Tesseract, Unzip, Rar,
+# Unrar, ClamAV, MeshLab, PopplerUtils, PDFTOTEXT, ImageMagick & xvfb-run.
+
+# <3 Open-Source
+# -----------------------------------------------------------------------------------
+
 # Helper script to download and run the build-ffmpeg script.
 
 make_dir () {
